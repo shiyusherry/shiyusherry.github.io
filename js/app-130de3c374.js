c948d24b38ad6d213f38b3e6895e2efb7d78be8c
yyypasserby <nixingwuwei@gmail.com>
2016-01-02 18:53:23 +0800

    Update 2016-01-02T10:46:48.489Z

--- js/app-130de3c374.js
+++
@@ -1,1 +0,0 @@
-!function(){"use strict";$.stellar.positionProperty.position={setTop:function(t,o,s){t.css("top",o)},setLeft:function(t,o,s){t.css("left",o)}},$.stellar({positionProperty:"position"})}();
